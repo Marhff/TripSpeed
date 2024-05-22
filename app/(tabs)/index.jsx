60168394c80c00b777c8e72a324af03062f532a1
Marcos França <marcosfranca310430@gmail.com>
2024-05-22 11:41:23 -0300

    Teste login config

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,14 +1,35 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import login from '../screens/LoginScreen'
 
 
 export default function HomeScreen() {
   return (
-   <View>
-    <Text>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit exercitationem officiis sint ex, quos illum eaque a! Laboriosam error, natus cupiditate unde at esse minus nam. Amet obcaecati ipsa recusandae!
-    </Text>
-   </View>
+    <View style={styles.container}>
+      <View style={{ height: 20 }} />
+      <Button
+        title="login"
+        onPress={() => console.log(navigation) }
+      />
+      <View style={{ height: 20 }} />
+      <Button
+        title="login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+});
