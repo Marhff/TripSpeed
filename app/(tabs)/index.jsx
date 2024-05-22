9b78337d71bbc29c30e0c59593d6eacdea72acc3
Marcos França <marcosfranca310430@gmail.com>
2024-05-22 00:48:29 -0300

    Config inicial do projeto

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,13 +1,13 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-   <text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum beatae quod tempore hic aspernatur? Quis numquam fuga est ducimus voluptates dolorem beatae ut suscipit tempore? Minima fugiat corporis possimus facere.</text>
+   <View>
+    <Text>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit exercitationem officiis sint ex, quos illum eaque a! Laboriosam error, natus cupiditate unde at esse minus nam. Amet obcaecati ipsa recusandae!
+    </Text>
+   </View>
   );
 }
 
---
+++ app/(tabs)/teste.jsx
@@ -0,0 +1,14 @@
+import {  View, Text } from 'react-native';
+
+
+export default function Teste() {
+  return (
+   <View>
+    <Text>
+      Pag teste 
+    </Text>
+   </View>
+  );
+}
+
+
